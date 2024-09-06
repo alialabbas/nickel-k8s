5f7a5851abcde46ba9a6abefbe994fe86fb2f21b
Ali Alabbas <ali.n.alabbas@gmail.com>
2024-09-06 07:55:07 -0400

    fully working, need to now clean up the code, model schema contract

--- generate.sh
+++ generate.sh
@@ -4,17 +4,13 @@ set -e
 GENERATED_FILE_NAME=generated.ncl
 FILENAME=./generated-k8s.ncl
 
-nickel eval $FILENAME | awk '{print substr($0, 2, length($0) - 2)}' > tmp.ncl
-
-# shellcheck disable=SC2046
-echo -e $(cat tmp.ncl) > $GENERATED_FILE_NAME
-
-rm tmp.ncl
+nickel eval $FILENAME | awk '{print substr($0, 2, length($0) - 2)}' >"$GENERATED_FILE_NAME"
 
 # Fix string formatting since nickel will always escape " and no other way to print this for now
-sed 's/m%\\"/m%"/g' $GENERATED_FILE_NAME -i
-sed 's/\\"\\%/"%/g' $GENERATED_FILE_NAME -i
-sed 's/\\"/"/g' $GENERATED_FILE_NAME -i
+sed 's/\\n/\n/g' "$GENERATED_FILE_NAME" -i
+sed 's/m%\\"/m%"/g' "$GENERATED_FILE_NAME" -i
+sed 's/\\"\\%/"%/g' "$GENERATED_FILE_NAME" -i
+sed 's/\\"/"/g' "$GENERATED_FILE_NAME" -i
 
 # TODO: consider using alternative string to escape
 # sed 's/\*\*REPLACEME\*\*/"%/g' $GENERATED_FILE_NAME -i
